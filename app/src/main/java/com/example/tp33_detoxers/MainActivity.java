28741f9b8da2fcebe3381150f748a63c0e8e0736
Sandy <hpan0013@student.monash.edu>
2020-08-26 22:37:23 +1000

    Test message v1.1

--- app/src/main/java/com/example/tp33_detoxers/MainActivity.java
+++ app/src/main/java/com/example/tp33_detoxers/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Hello Xin
+        //Hello Xin Xu
     }
 
     public void open(){
