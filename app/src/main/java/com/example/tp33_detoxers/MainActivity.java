9534a66828649a1fae6b4cf2600262ba8b0dd240
xxuu0040 <xuxinchn39@gmail.com>
2020-08-27 00:38:21 +1000

    bottom navigation bar

--- app/src/main/java/com/example/tp33_detoxers/MainActivity.java
+++ app/src/main/java/com/example/tp33_detoxers/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.tp33_detoxers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +16,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Hello Xin Xu
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavigation);
+
     }
 
-    public void open(){
-        //R.id.sawtooth;
-    }
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavigation=new
+            BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    int id = item.getItemId();
+                    Fragment fragment = null;
+
+                    switch (id){
+                        case R.id.page_category:
+                            fragment = new CategoryFragment();
+                            break;
+                        case  R.id.page_home:
+                            fragment = new HomeFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                    return true;
+                }
+
+
+    };
+
+
 }
